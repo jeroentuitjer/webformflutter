93cd080d88a4681a55b21179749af4e5b81c4887
Jeroen Tuitjer <jeroen.tuitjer@ahold.com>
2020-12-23 14:32:09 +0100

    Form with output in the terminal

--- lib/ui/formattr/textfield.dart
+++ lib/ui/formattr/textfield.dart
@@ -6,90 +6,100 @@ class SubscribeForm extends StatefulWidget {
 }
 
 class _FormState extends State<SubscribeForm> {
+  var formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
     return Container(
-      width: 500,
+      width: 400,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
       child: Column(
         children: [
-          // Form(
-          // child: GenderChoice()
-          // ),
           Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Voornaam',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Achternaam',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Postcode',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Huisnummer',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Toev.',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'E-mailadres',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Wachtwoord',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Telefoonnummer',
-              ),
-            ),
-          ),
-          Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: '',
-                labelText: 'Geboortedatum',
-              ),
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Voornaam',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Achternaam',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Postcode',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Huisnummer',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Toev.',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'E-mailadres',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Wachtwoord',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Telefoonnummer',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+                TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: '',
+                      labelText: 'Geboortedatum',
+                    ),
+                    onSaved: (String value) {
+                      print('Value: $value');
+                    }),
+              ],
             ),
           ),
           RaisedButton(
             child: Text('Terug'),
           ),
           RaisedButton(
+            onPressed: () {
+              formKey.currentState.save();
+            },
             child: Text("Meld je aan"),
           )
         ],
